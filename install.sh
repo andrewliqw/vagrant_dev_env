2b3765c18bd74e22750889e38a7a43be9173b88c
Andrew Li <andrewliqw@outlook.com>
2017-06-02 17:09:35 +1000

    backup for 2/6/2017

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 # apt-get -y update && apt-get -y upgrade && \
-# apt-get -y install build-essential
-# apt-get -y install python3-venv python3-dev
-# apt-get -y install postgresql
+apt-get -y install build-essential
+apt-get -y install python3-venv python3-dev
+apt-get -y install postgresql
 apt-get -y install apache2
 # apt-get -y install git
 
@@ -14,11 +14,16 @@ apt-get -y install apache2
 # apache2
 if ! [ -L /var/www ]; then
   rm -rf /var/www
-  mkdir -p /vagrant/shared/www && ln -fs /vagrant/shared/www /var/www
+  mkdir -p /vagrant/shared/www/html && ln -fs /vagrant/shared/www /var/www
 fi
 
 # postgres
-
+if ! [ -L /var/lib/postgresql ]; then
+  rsync -av /var/lib/postgresql /vagrant/shared
+  mv /var/lib/postgresql /var/lib/postgresql.bak
+  ln -fs /vagrant/shared/postgresql /var/lib/postgresql
+  chown postgres:postgres /var/lib/postgresql
+fi
 
 # create python 3 virtual environment
 # cd /home/ubuntu && rm -rf /home/ubuntu/py35venv && python3 -mvenv --copies py35venv && /home/ubuntu/py35venv/bin/python -mpip install -U pip
